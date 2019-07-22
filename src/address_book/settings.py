71598f152edd85a795de3f92f779121aeb474087
Mauricio Romero <mromeroval@ucdavis.edu>
2019-07-21 23:09:30 -0700

    Add static root variable

--- src/address_book/settings.py
+++ src/address_book/settings.py
@@ -133,6 +133,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
                     os.path.join(BASE_DIR, "static"),
                     ]
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"staticfiles")
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
